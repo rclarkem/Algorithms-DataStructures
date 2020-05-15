858e4fc9aabbd0141fc8c3a3102c8b1f25886401
Reina <r.clarkemitchell@gmail.com>
2020-05-15 16:55:23 -0400

    practice

--- js_practice/AlgoPractice.js
+++ js_practice/AlgoPractice.js
@@ -185,3 +185,93 @@ function myMnemonic(str1, str2, str3, str4) {
 	return newStr
 }
 myMnemonic('Must', 'Accept', 'Sugar', 'Happiness') // => MASH
+
+var contacts = [
+	{
+		firstName: 'Akira',
+		lastName: 'Laine',
+		number: '0543236543',
+		likes: ['Pizza', 'Coding', 'Brownie Points'],
+	},
+	{
+		firstName: 'Harry',
+		lastName: 'Potter',
+		number: '0994372684',
+		likes: ['Hogwarts', 'Magic', 'Hagrid'],
+	},
+	{
+		firstName: 'Sherlock',
+		lastName: 'Holmes',
+		number: '0487345643',
+		likes: ['Intriguing Cases', 'Violin'],
+	},
+	{
+		firstName: 'Kristian',
+		lastName: 'Vos',
+		number: 'unknown',
+		likes: ['JavaScript', 'Gaming', 'Foxes'],
+	},
+]
+
+function lookUpProfile(name, prop) {
+	for (let i = 0; i < contacts.length; i++) {
+		if (contacts[i].firstName === name) {
+			if (contacts[i].hasOwnProperty(prop)) {
+				return contacts[i][prop]
+			} else {
+				return 'No such property'
+			}
+		}
+	}
+	return 'No such contact'
+	// Only change code above this line
+}
+
+lookUpProfile('Bob', 'potato')
+
+function sum(arr, n) {
+	// Only change code below this line
+	if (n <= 0) {
+		return 0
+	} else {
+		return sum(arr, n - 1) + arr[n - 1]
+	}
+
+	// Only change code above this line
+}
+sum([2, 3, 4], 2) // 2
+
+var collection = {
+	2548: {
+		album: 'Slippery When Wet',
+		artist: 'Bon Jovi',
+		tracks: ['Let It Rock', 'You Give Love a Bad Name'],
+	},
+	2468: {
+		album: '1999',
+		artist: 'Prince',
+		tracks: ['1999', 'Little Red Corvette'],
+	},
+	1245: {
+		artist: 'Robert Palmer',
+		tracks: [],
+	},
+	5439: {
+		album: 'ABBA Gold',
+	},
+}
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+	if (value === '') {
+		delete collection[id][prop]
+	} else if (prop === 'tracks') {
+		collection[id][prop] = collection[id][prop] || []
+		collection[id][prop].push(value)
+	} else {
+		collection[id][prop] = value
+	}
+	return collection
+}
+
+updateRecords(1245, 'tracks', 'Take a Chance on Me')
