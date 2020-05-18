8c187648ecaf369ff6ca5a9f146d7d94011270ad
Reina <r.clarkemitchell@gmail.com>
2020-05-18 18:24:21 -0400

    more practice

--- js_practice/AlgoPractice.js
+++ js_practice/AlgoPractice.js
@@ -275,3 +275,716 @@ function updateRecords(id, prop, value) {
 }
 
 updateRecords(1245, 'tracks', 'Take a Chance on Me')
+
+
+
+   
+var arr1 = [5, 10, 15, 20];
+var arr2 = [15, 88, 1, 5, 7];
+var arr3 = [1, 10, 15, 5, 20];
+function intersection (arr){
+return arr.reduce(function(acc, curr) {
+    console.log("acc:", acc,'curr:', curr)
+		let result = [];
+		   for(var i = 0; i < acc.length; i++){
+        if(curr.includes(acc[i])){
+          result.push(acc[i]);
+        }
+      }
+      return result;
+	})
+}
+intersection([arr1, arr2, arr3]); // should log: [5, 15]
+
+
+ 
+
+ 
+var arr1 = [5, 10, 15];
+var arr2 = [15, 88, 1, 5, 7];
+var arr3 = [100, 15, 10, 1, 5];
+
+function union (arr){
+let flattened = arr.reduce(function(acc, curr){
+	return acc.concat(curr)
+})
+return [...new Set(flattened)]
+}
+
+union([arr1, arr2, arr3]); // should log: [5, 10, 15, 88, 1, 7, 100]
+
+
+function getRemainder(num1, num2) {
+  //your code goes here...
+if(num1 > num2){
+return 	num1 % num2
+} else {
+return num2 % num1
+}
+}
+console.log(getRemainder(17, 5)); //=> 2
+
+function gradeCalculator(grade) {
+let result;
+if(grade >= 90){
+	result = "A"
+} else if(grade >= 80){
+	result = "B"
+}  else if(grade >= 70){
+	result = "C"
+}  else if(grade >= 60){
+	result = "D"
+}  else if(grade <= 50){
+	result = "F"
+}
+return result
+}
+
+Uncomment the lines below to test your code
+
+console.log(gradeCalculator(92)); //=> "A"
+console.log(gradeCalculator(84)); //=> "B"
+console.log(gradeCalculator(70)); //=> "C"
+console.log(gradeCalculator(61)); //=> "D"
+console.log(gradeCalculator(43)); //=> "F"
+
+function arrayBuilder(obj) {
+let newArr = []
+for(let key in obj){
+	for(let i = 0; i < obj[key]; i++){
+		newArr.push(key)
+	}
+}
+return newArr
+}
+
+   
+
+console.log(arrayBuilder({'cats': 2, 'dogs': 1})); //=> ['cats', 'cats', 'dogs']
+console.log(arrayBuilder({})); //=> []
+
+
+   
+
+   
+var arr1 = ['hi', 'howdy', 'bye', 'later', 'hello'];
+var arr2 = ['HI', 'Howdy', 'BYE', 'later', 'HELLO'];
+function uppercaser(str) { return str.toUpperCase(); }
+
+function objOfMatches(arr1, arr2, callback){
+let newObj = {}
+let check = arr1.filter(function(val){
+	return arr2.includes(val.toUpperCase()) 
+})
+console.log(check)
+for(let i = 0; i < check.length; i++){
+	newObj[check[i]] = check[i].toUpperCase()
+}
+return newObj
+}
+objOfMatches(arr1, arr2, uppercaser); // should log: { hi: 'HI', bye: 'BYE', hello: 'HELLO' }
+
+
+   
+
+   
+function uppercaser(str) { return str.toUpperCase(); }
+function capitalize(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }
+function repeater(str) { return str + str; }
+var items = ['catfood', 'glue', 'beer'];
+var functions = [uppercaser, capitalize, repeater];
+
+function multiMap(arr, functionsArr){
+let newArr = {}
+let test = arr.map(function(val){
+	return functionsArr.map(function(val2){
+		return val2(val)
+	})
+})
+for(let i = 0; i < test.length; i++){
+	newArr[arr[i]] = test[i]
+}
+return newArr
+}
+console.log(multiMap(items, functions)); 
+should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
+
+   
+function majority(arr, callback){
+  let filtered = arr.map(function(val){
+    return callback(val)
+  })
+
+let trueValues = filtered.filter(function(val2){
+		return val2 === true
+	})
+let falseValues = filtered.filter(function(val3){
+		return val3 === false
+})
+if(trueValues.length > falseValues.length){
+			return true
+		} else if(trueValues.length === falseValues.length){
+			return false
+		} else {
+			return false
+		}
+
+}
+   
+var isOdd = function(num) { return num % 2 === 1; };
+console.log(majority([1, 2, 3, 4, 5], isOdd)); // should log: true
+// console.log(majority([2, 3, 4, 5], isOdd)); // should log: false
+
+
+function prioritize(arr, callback){
+let trueValues = arr.filter(function(val2){
+	return callback(val2) === true
+})
+	console.log(trueValues)
+let falseValues = arr.filter(function(val3){
+		 return callback(val3) === false
+
+})
+// console.log(falseValues)
+return trueValues.concat(falseValues)
+}
+//    
+function startsWithS(str) { return str[0].toLowerCase() === 's'; }
+var tvShows = ['curb', 'rickandmorty', 'seinfeld', 'sunny', 'friends']
+console.log(prioritize(tvShows, startsWithS)); // should log: ['seinfeld', 'sunny', 'curb', 'rickandmorty', 'friends']
+
+
+   
+function countBy(arr, callback){
+	let newObj = {}
+	let props = arr.map(function(val){
+		return callback(val)
+	})
+
+for(let i = 0; i < props.length; i++){
+	if(newObj[props[i]]){
+		newObj[props[i]]++
+	} else {
+		newObj[props[i]] = 1
+	}
+}
+return newObj
+}
+//    
+function evenOdd(n) {
+  if (n % 2 === 0) return 'even';
+  else return 'odd';
+}
+var nums = [1, 2, 3, 4, 5];
+console.log(countBy(nums, evenOdd)); // should log: { odd: 3, even: 2 }
+
+
+   
+var decimals = [1,2,3,4,5].sort();
+// console.log(decimals)
+var floored = function(num) { return Math.floor(num); };
+var evenOdd = function(num) { return num % 2 === 0; };
+
+function groupBy(arr, callback){
+debugger
+let newObj = {}
+let test = arr.map(function(val){
+		return callback(val)
+	})
+for(let i = 0; i < test.length; i++){
+	if(!newObj[test[i]]){
+				 newObj[test[i]] = [arr[i]]
+	} else {
+				newObj[test[i]].push(arr[i])
+			}
+}	
+return newObj
+}
+groupBy(decimals, floored); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+groupBy(decimals, evenOdd)
+
+for(let i = 0; i < mapped.length; i++){
+	if(!newObj[mapped[i]]){
+		if(Math.floor(arr[i])=== mapped[i]){
+			 newObj[mapped[i]] = [arr[i]]
+		}
+	} else {
+		if(Math.floor(arr[i]) === mapped[i]){
+			newObj[mapped[i]].push(arr[i])
+	}
+}
+
+var sunny = { mac: 'priest', dennis: 'calculating', charlie: 'birdlaw', dee: 'bird', frank: 'warthog'};
+
+function goodKeys(obj, callback){
+  let base = [];
+	let callbackResult;
+	for(prop in obj){
+	callbackResult = callback(obj[prop])
+if(callbackResult === true){
+	base.push(prop)
+	}
+}
+		return base
+		}
+
+
+//    
+
+function startsWithBird(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
+
+
+
+console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
+
+   
+function createFunction (){
+  function helloWorld(){
+   return "Hello World"
+  }
+return helloWorld
+}
+//    
+var myFunction = createFunction();
+myFunction(); //should log: 'hello world'
+
+
+function addByX(input){
+ function add(x){
+    return x + input
+  }
+return add
+
+}
+const addByTwo = addByX(2);
+console.log(addByTwo(2))
+console.log(addByTwo(3))
+
+function partlist(arr) {
+    debugger
+		var newArray;
+    var returnArray=[];
+    for( var i=1; i<arr.length; i++){
+      newArray = [];
+    //  console.log( newArray.push(arr.slice(0,4).join(" ")))
+		// console.log(newArray.push(arr.slice(4).join(' ')))
+		console.log(newArray)
+		}
+}
+			partlist(["az", "toto", "picaro", "zone", "kiwi"])
+
+
+function censor(){
+	let storedStrings1 = {}
+	function censored(str1, str2){
+		if(arguments.length === 2){
+			// console.log(arguments.length)
+		storedStrings1[str1] = str2
+			// storedStrings1[str1] = str2
+			// console.log(storedStrings1)
+
+		} else{
+			for(let prop in storedStrings1){
+				console.log(str1 ,'str1')
+				str1 = str1.replace(prop, storedStrings1[prop])
+			}
+			return str1
+		}
+
+// storedStrings2.push(str1)
+
+// console.log(storedStrings2.length)
+
+			// console.log(arguments)
+			// console.log(storedStrings2)	
+
+
+	}
+	return censored
+}
+
+
+
+
+//    
+const changeScene = censor();
+changeScene('dogs', 'cats');
+console.log(changeScene('quick', 'slow'));
+console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // should log: 'The slow, brown fox jumps over the lazy cats.'
+storedStrings.push(...args)
+console.log(storedStrings)
+console.log(storedStrings)
+// if two strings are given, this function will store the two strings 
+// if given one string, it will return the given one string and replace all instances of the pair in the one string given
+
+
+function makePerson(name, age) {
+let obj = {name: name,
+ age: age};
+
+return obj
+
+
+}
+
+ const vicky = makePerson('Vicky', 24);
+
+
+// Uncomment these lines to check your work!
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
+
+
+
+const personStore = {
+  greet: function() {
+    console.log('hello');
+  }
+}
+
+function personFromPersonStore(name, age) {
+	const person = Object.create(personStore);
+  person.name = name;
+  person.age = age;
+  return person;
+}
+
+const sandra = personFromPersonStore('Sandra', 26);
+personStore.introduce = function() {
+	console.log(`Hi, my name is ${this.name}`);
+	}
+//    
+
+
+// Uncomment this line to check your work!
+sandra.introduce(); // -> Logs 'Hi, my name is Sandra'
+
+
+function PersonConstructor() {
+  this.greet = function() {
+    console.log('hello');
+  }
+}
+
+function personFromConstructor(name, age) {
+  let person = new PersonConstructor(name, age);
+  person.name = name;
+  person.age = age;
+  return person;
+}
+var simon = new PersonConstructor;
+
+// function personFromConstructor(name, age) {
+//   let person = new PersonConstructor(name, age);
+//   person.name = name;
+//   person.age = age;
+//   return person;
+// }
+// var simon = new PersonConstructor;
+
+
+
+const mike = personFromConstructor('Mike', 30);
+
+// Uncomment these lines to check your work!
+console.log(mike.name); // -> Logs 'Mike'
+console.log(mike.age); // -> Logs 30
+mike.greet(); // -> Logs 'hello'
+
+
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+  sayLegs: function(){
+    return `This dog has ${dog.numLegs} legs.`
+  }
+};
+
+dog.sayLegs();
+
+function pluralize(arr){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+   newArr.push(arr[i] + "s")
+  }
+	return newArr
+}
+//    
+const animals = ["dog", "cat", "tree frog"];
+console.log(pluralize(animals));
+
+
+function subtractTwo(num){
+   return num - 2 
+}
+	function map(arr,callback){
+  let newArr = []
+  for(let i = 0; i < arr.length; i++){
+				newArr.push(callback(arr[i]))
+			}
+
+
+  return newArr
+}
+console.log(map([3,4,5], subtractTwo))
+
+
+//    
+function groupBy(array,callback){
+  let arr = []
+  let obj = {};
+let called = array.map(function(val){
+		return callback(val)
+	})
+	for(let i = 0; i < called.length; i++){
+		if(!obj[called[i]]){
+			obj[called[i]] = [array[i]]
+		} else {
+			obj[called[i]].push(array[i])
+		}
+
+
+}
+	return obj
+}
+//    
+const decimals = [1.3, 2.1, 2.4];
+const floored = function(num) { return Math.floor(num); };
+console.log(groupBy(decimals, floored)); // should log: { 1: [1.3], 2: [2.1, 2.4] }
+
+
+   
+function goodKeys(obj,callback){
+let newArr = []
+for(let props in obj){
+if(callback(obj[props])){
+	newArr.push(props)
+}
+}
+return newArr
+}
+//    
+const sunny = { mac: 'priest', dennis: 'calculator', charlie: 'birdlaw', dee: 'bird', frank: 'warthog' };
+function startsWithBird(str) { return str.slice(0, 4).toLowerCase() === 'bird'; };
+console.log(goodKeys(sunny, startsWithBird)); // should log: ['charlie', 'dee']
+
+//    
+function createFunction (){
+  function helloWorld(){
+   return "hello world"
+  }
+return helloWorld
+}
+//    
+var myFunction = createFunction();
+myFunction(); //should log: 'hello world'
+
+//    
+function createFunctionWithInput(input){
+  function returnInput(){
+    return input
+  }
+  return returnInput
+}
+//    
+const sampleFunc = createFunctionWithInput('sample');
+console.log(sampleFunc()); // should log: 'sample'
+const helloFunc = createFunctionWithInput('hello');
+console.log(helloFunc()); // should log: 'hello'
+
+
+function outer() {
+  var counter = 0; // this variable is outside incrementCounter's scope
+  function incrementCounter() {
+    counter++;
+    console.log('counter', counter);
+  }
+  return incrementCounter;
+}
+
+var willCounter = outer();
+var jasCounter = outer();
+
+// Uncomment each of these lines one by one.
+// Before your do, guess what will be logged from each function call.
+
+willCounter();
+willCounter();
+willCounter();
+
+// jasCounter();
+// willCounter();
+
+
+//    
+function addByX(input){
+ function add(x){
+    return x + input
+  }
+return add
+
+}
+const addByTwo = addByX(2);
+
+// Now call addByTwo with an input of 1 and log the output
+
+// Now call addByTwo with an input of 2 and log the output
+
+
+   
+function once(callback){
+let functionCall = false
+let result;
+
+ function onceIfied(...args){
+   if(functionCall === false){
+     result = callback(...args)
+     functionCall = true
+   }
+   return result
+ } 
+  return onceIfied
+}
+const addByTwoOnce = once(function(num) {
+  return num + 2;
+});
+
+   
+// should log 7
+
+
+   
+function after(num, callback){
+let counter = 0  
+
+  function call (){
+
+  }
+
+}
+const called = function(string) { return('hello ' + string); };
+const afterCalled = after(3, called);
+
+// UNCOMMENT THESE LINES TO TEST YOUR WORK
+console.log(afterCalled('world')); // -> nothing is printed
+console.log(afterCalled('world')); // -> nothing is printed
+console.log(afterCalled('world')); // -> 'hello world' is printed
+
+
+
+   
+function once(callback){
+  let result;
+  function test(b){
+		console.log(b)
+    if(result === undefined){
+      result = callback(b)
+    }
+    return result
+  }
+  return test
+}
+const addByTwoOnce = once(function(num) {
+  return  num + 2;
+});
+
+//    
+console.log(addByTwoOnce(5));  //should log 7
+console.log(addByTwoOnce(10));  //should log 7
+console.log(addByTwoOnce(9001));  //should log 7
+
+
+   
+   
+function after(numOfTimes, callback){
+  let timesCalled = 1
+  let result
+  function numberCalled(s){
+    if(numOfTimes === 0){
+      return callback(s)
+    } else {
+       if(timesCalled === numOfTimes){
+      result = callback(s)
+    } else {
+		timesCalled++
+  }
+	return result
+
+}
+
+}
+  return numberCalled
+}
+const called = function(string) { return('hello ' + string); };
+const afterCalled = after(3, called);
+
+// UNCOMMENT THESE LINES TO TEST YOUR WORK
+console.log(afterCalled('world')); // -> nothing is printed
+console.log(afterCalled('world')); // -> nothing is printed
+console.log(afterCalled('world')); // -> 'hello world' is printed
+
+
+
+function saveOutput(callback,string){
+  let password = {}
+  function replicate(s){
+    if(typeof s !== 'string'){
+			password[s] = callback(s)
+		} else {
+			return password
+		}
+		return password[s]
+
+  }
+	return replicate
+}
+//    
+const multiplyBy2 = function(num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // should log: 4
+console.log(multBy2AndLog(9)); // should log: 18
+console.log(multBy2AndLog('boo')); // should log: { 2: 4, 9: 18 }'
+
+
+   
+function cycleIterator(array){
+let storedElem = 0 
+  function test (){
+		storedElem++
+if(storedElem === array.length + 1){
+	storedElem = 1
+}
+return array[storedElem - 1]
+	}
+	return test
+
+}
+
+//    
+const threeDayWeekend = ['Fri', 'Sat', 'Sun'];
+const getDay = cycleIterator(threeDayWeekend);
+console.log(getDay()); // should log: 'Fri'
+console.log(getDay()); // should log: 'Sat'
+console.log(getDay()); // should log: 'Sun'
+console.log(getDay()); // should log: 'Fri'
+
+function censor() {
+	let stored = {}
+	function test(...args) {
+		if (arguments.length === 2) {
+			stored[arguments[0]] = arguments[1]
+		} else {
+			for (let props in stored) {
+				arguments[0] = arguments[0].replace(props, stored[props])
+			}
+			return arguments[0]
+		}
+	}
+	return test
+}
+
+const changeScene = censor();
+changeScene('dogs', 'cats');
+changeScene('quick', 'slow');
+console.log(changeScene('The quick, brown fox jumps over the lazy dogs.')); // should log: 'The slow, brown fox jumps over the lazy cats.'
