eafdd390ac97163d01ce8d7d2ca069f8820f2240
Reina <r.clarkemitchell@gmail.com>
2020-07-31 22:47:46 -0400

    finished first 4 sections of part 4

--- js_practice/HR_practice_problems.js
+++ js_practice/HR_practice_problems.js
@@ -704,3 +704,81 @@ function findShortestElement(arr) {
   }
   return smallest;
 }
+
+/*
+Write a function called "getLargestElement".
+
+Given an array, "getLargestElement" returns the largest number in the given array.
+
+var output = getLargestElement([5, 2, 8, 3]);
+console.log(output); // --> 8;
+*/
+function getLargestElement(arr) {
+   return arr.sort((a,b) => b - a)[0]
+}
+
+/*
+Write a function called "computeSumOfAllElements".
+
+Given an array of numbers, "computeSumOfAllElements" returns the sum of all the elements in the given array.
+
+var output = computeSumOfAllElements([1, 2, 3])
+console.log(output); // --> 6
+*/
+
+function computeSumOfAllElements(arr) {
+  return arr.reduce((acc,curr) => acc + curr , 0)
+}
+
+
+/*
+Write a function called "getElementsThatEqual10AtProperty".
+
+Given an object and a key, "getElementsThatEqual10AtProperty" returns an array containing all the elements of the array located at the given key that are equal to ten.
+
+Notes:
+
+If the array is empty, it should return an empty array.
+If the array contains no elements that are equal to 10, it should return an empty array.
+If the property at the given key is not an array, it should return an empty array.
+If there is no property at the key, it should return an empty array.
+var obj = {
+  key: [1000, 10, 50, 10]
+};
+var output = getElementsThatEqual10AtProperty(obj, 'key');
+console.log(output); // --> [10, 10]
+*/
+function getElementsThatEqual10AtProperty(obj, key) {
+   if(obj.hasOwnProperty(key)){
+		if(Array.isArray(obj[key])){
+			return obj[key].filter(num => num === 10)
+		}
+	}
+	return []
+}
+
+/*
+Write a function called "getElementsLessThan100AtProperty".
+
+Given an object and a key, "getElementsLessThan100AtProperty" returns an array containing all the elements of the array located at the given key that are less than 100.
+
+Notes:
+
+If the array is empty, it should return an empty array.
+If the array contains no elements less than 100, it should return an empty array.
+If the property at the given key is not an array, it should return an empty array.
+If there is no property at the key, it should return an empty array.
+var obj = {
+  key: [1000, 20, 50, 500]
+};
+var output = getElementsLessThan100AtProperty(obj, 'key');
+console.log(output); // --> [20, 50]
+*/
+function getElementsLessThan100AtProperty(obj, key) {
+  	if(obj.hasOwnProperty(key)){
+		if(Array.isArray(obj[key])){
+			return obj[key].filter(num => num < 100)
+		}
+	}
+	return []
+}
