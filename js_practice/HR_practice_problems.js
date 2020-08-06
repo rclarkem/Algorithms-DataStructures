b9e8ce23d71650813e9aeaf9df953f32d0b78485
Reina <r.clarkemitchell@gmail.com>
2020-08-06 18:53:55 -0400

    Finished Part 4

--- js_practice/HR_practice_problems.js
+++ js_practice/HR_practice_problems.js
@@ -1100,3 +1100,111 @@ function getEvenElementsAtProperty(obj, key) {
   }
   return [];
 }
+
+/*
+Write a function called "getSmallestElementAtProperty".
+
+Given an object and a key, "getSmallestElementAtProperty" returns the smallest element in the array located at the given key.
+
+Notes:
+
+If the array is empty, it should return undefined.
+If the property at the given key is not an array, it should return undefined.
+If there is no property at the key, it should return undefined.
+var obj = {
+  key: [2, 1, 5]
+};
+var output = getSmallestElementAtProperty(obj, 'key');
+console.log(output); // --> 1
+*/
+function getSmallestElementAtProperty(obj, key) {
+  const arr = obj[key];
+  if (obj.hasOwnProperty(key) && Array.isArray(arr)) {
+    if (arr.length > 1) {
+      return arr.slice().sort((a, b) => a - b)[0];
+    }
+  }
+  return undefined;
+}
+
+/*
+Write a function called "getLargestElementAtProperty".
+
+Given an object and a key, "getLargestElementAtProperty" returns the largest element in the array located at the given key.
+
+Notes:
+
+If the array is empty, it should return undefined.
+If the property at the given key is not an array, it should return undefined.
+If there is no property at the key, it should return undefined.
+var obj = {
+  key: [1, 2, 4]
+};
+var output = getLargestElementAtProperty(obj, 'key');
+console.log(output); // --> 4
+*/
+
+function getLargestElementAtProperty(obj, key) {
+  const arr = obj[key];
+  if (obj.hasOwnProperty(key) && Array.isArray(arr)) {
+    if (arr.length > 1) {
+      return arr.slice().sort((a, b) => b - a)[0];
+    }
+  }
+  return undefined;
+}
+
+/*
+Write a function called "getAllButLastElementOfProperty".
+
+Given an object and a key, "getAllButLastElementOfProperty" returns an array containing all but the last element of the array located at the given key.
+
+Notes:
+
+If the array is empty, it should return an empty array.
+If the property at the given key is not an array, it return an empty array.
+If there is no property at the key, it should return an empty array.
+var obj = {
+  key: [1, 2, 3]
+};
+var output = getAllButLastElementOfProperty(obj, 'key');
+console.log(output); // --> [1,2]
+*/
+
+function getAllButLastElementOfProperty(obj, key) {
+  const arr = obj[key];
+  if (obj.hasOwnProperty(key) && Array.isArray(arr)) {
+    if (arr.length > 1) {
+      return arr.slice(0, arr[arr.length - 2]);
+    }
+  }
+  return [];
+}
+
+/*
+Write a function called "getElementOfArrayProperty".
+
+Given an object, a key, and a numerical index, "getElementOfArrayProperty" returns the value of the element at the given index of the array located within the given object at the given key.
+
+Notes:
+
+If the array is empty, it should return undefined.
+If the given index is out of range of the array located at the given key, it should return undefined.
+If the property at the given key is not an array, it should return undefined.
+If there is no property at the key, it should return undefined.
+var obj = {
+ key: ['Jamil', 'Albrey']
+};
+var output = getElementOfArrayProperty(obj, 'key', 0);
+console.log(output); // --> 'Jamil'
+
+*/
+function getElementOfArrayProperty(obj, key, index) {
+  const arr = obj[key];
+  if (obj.hasOwnProperty(key) && Array.isArray(arr)) {
+    if (arr.length > 1) {
+      return arr.slice()[index];
+    }
+  }
+  return undefined;
+}
